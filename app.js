2595766d0ff2820e4baa3a0ed3886357c9291867
elene638 <elenejavaxishvili@yahoo.com>
2022-07-24 21:17:13 -0700

    Add files

---
+++ app.js
@@ -0,0 +1,153 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//create new database
+mongoose.connect("mongodb+srv://admin-elene:test123@cluster0.xrkkl.mongodb.net/todolistDB");
+
+//new schema
+const itemsSchema = {
+  name: String
+};
+
+//new mongoose model (with capital letter), based on schema
+const Item = mongoose.model("Item", itemsSchema);
+//we should pass in newListItems items from Item model 
+
+const item1 = new Item ({
+  name: "Welcome to your to do list!"
+});
+
+const item2 = new Item ({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item.>"
+});
+
+//put items in array
+const defaultItems = [item1, item2, item3];
+
+//we can use mongoose method insertMany(), to insert above array in one go in a collection, in insert item first parameter is what we won't to insert
+
+//new schema for custom list
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+//mongoose new model
+
+const List = mongoose.model("List", listSchema);
+
+app.get("/", function(req, res) {
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfull");
+        }
+ 
+      });
+      res.redirect("/")
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+
+  });
+
+});
+
+app.get("/:customListName", function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+  //to find a list that is already existing or not
+  List.findOne({name: customListName}, function (err, foundList) {
+    if(!err){
+      if (!foundList) {
+        //create new list
+        //new list document
+        const list = new List ({
+          name: customListName,
+          items: defaultItems
+        });
+
+        list.save();
+        res.redirect("/" + customListName)
+      } else {
+        //show an existing list
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+  const item = new Item ({
+    name: itemName
+  });
+
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/")
+  } else {
+    List.findOne({name: listName}, function (err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName)
+    });
+  }
+});
+
+app.post("/delete", function(req,res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      if (!err) {
+        console.log("Success");
+        res.redirect("/"); //will find all the items and it will render the page so deleted item won't be there anymore
+      }
+    });
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function (err, foundList) {
+      if (!err){
+        res.redirect("/" + listName)
+      }
+    })
+  }
+
+  
+});
+
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
+});
